050887411235af2885df38326b6348095a86077c
Gabriel Koehler da Silva <gabrielks@weg.net>
2023-11-10 21:49:30 -0300

    instalação do prime vue

---
+++ src/components/datePick.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Calendar v-model="date" selectionMode="range" :manualInput="false" showButtonBar showIcon />
+       <h1>{{date}}</h1>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+let date=ref('');
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
