5e1c4a1492249279a820d7cdae4653ef7b9e0149
Henrique Goncalves de Jesus <henriquegj@weg.net>
2023-11-27 19:18:11 -0300

    popUp propriedade quase pronto

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Tabelas from './components/Tabelas.vue';
-</script>
-
-<template>
-  <RouterView/>
 import Input from './components/Input.vue'
 
 import fundoPopUp from './components/fundoPopUp.vue';
@@ -13,22 +9,12 @@ import {funcaoPopUpStore} from './stores/funcaoPopUp'
 import PopUpCriaPropriedade from './components/popUpCriaPropriedade.vue';
 
 const funcaoPopUp = funcaoPopUpStore();
- 
 </script>
 
 <template>
-  <RouterView />
-  <div class="w-full h-screen bg-red-50">
-    <PopUpCriaPropriedade></PopUpCriaPropriedade>
-  <button @click="funcaoPopUp.abrePopUp()">
-    teste
-  </button>
+  <RouterView/>
+  <PopUpCriaPropriedade></PopUpCriaPropriedade>
+  <button @click="funcaoPopUp.abrePopUp">teste</button>
 </template>
-
-<style scoped>
-
-  
-
-</style>
 
 
--- src/components/popUpCriaPropriedade.vue
+++ src/components/popUpCriaPropriedade.vue
@@ -20,12 +20,18 @@
                         <selectPadrao :listaSelect="listaSelecao" fonteTamanho="2.5" largura="20vh" altura="6vh"></selectPadrao>
                     </div>
                 </div>
-                <div class="flex justify-end w-[88%] h-[15%] p-4 items-end">
-                    <Botao preset="PadraoVazado" texto="CRIAR" tamanho-da-borda="4px" tamanhoPadrao="personalizado" width="20vh" height="6vh" tamanhoDaFonte="3vh" sombras='nao' :funcaoClick="cria" ></Botao>
+                <div class="alinhaBotoes">
+                    <div class="flex items-center justify-start" @click="limpaTudo()">
+                        <Botao preset="Sair" width="20vh" height="6vh" tamanhoDaFonte="3vh" tamanhoPadrao="personalizado" :funcaoClick="funcaoPopUp.fechaPopUp" ></Botao>
+                    </div>
+                    <div class="flex items-center justify-end">
+                        <Botao preset="PadraoVazado" texto="CRIAR" tamanho-da-borda="4px" tamanhoPadrao="personalizado" width="20vh" height="6vh" tamanhoDaFonte="3vh" sombras='nao' :funcaoClick="cria" ></Botao>
+                    </div>
+                   
                 </div>
             </div>
         </FundoPopUp>
-
+        <!-- VERIFICAR TIPO DE VIZUALIZAÇÃOES PARA CADA TIPO DE PROPRIEDADE -->
     </div>
 </template>
 
@@ -36,9 +42,12 @@ import selectPadrao from './selectPadrao.vue';
 import Botao from './Botao.vue';
 import { criaPropriedadeStore } from '../stores/criaPropriedade';
 import { onMounted, ref } from 'vue';
+import {funcaoPopUpStore} from '../stores/funcaoPopUp'
 let listaSelecao;
 let nomePropriedade=ref("");
 let tipo=ref("");
+const funcaoPopUp= funcaoPopUpStore();
+
 function cria(){
     
     if(tipo.value==''){
@@ -52,7 +61,10 @@ onMounted(()=>{
         }
         
     )
-
+   function limpaTudo(){
+     nomePropriedade=""
+     tipo=""
+   }
 </script>
 
 <style >
@@ -63,6 +75,9 @@ onMounted(()=>{
         @apply w-[80%] h-[11%]  gap-4  items-center justify-items-center justify-center ; 
         display: grid;
         grid-template-columns: 40% 55%;
+    }
+    .alinhaBotoes{
+        @apply grid grid-cols-2 justify-center items-center w-[80%] gap-16 p-4 ;
     }
 }
 
