45a235ea1463cdc93287a29728e96efea95f063f
Nicolas Stassun <stassunn@weg.net>
2023-11-28 21:53:14 -0300

    funcionando

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,15 @@ import {funcaoPopUpStore} from './stores/funcaoPopUp'
 import PopUpCriaPropriedade from './components/popUpCriaPropriedade.vue';
 import Convite from './components/convite.vue';
 import ListaConvidados from './components/ListaConvidados.vue';
+import Botao from './components/Botao.vue';
+import checkBox from './components/checkBox.vue';
 
 const funcaoPopUp = funcaoPopUpStore();
 </script>
 
-<template>
-
-  <RouterView />
-  <ListaConvidados></ListaConvidados>
+<template >
+  <div class="bg-slate-500 w-full h-[100vh]">
+    <checkBox tamanho="grande"></checkBox>
+  </div>
+  
 </template>
