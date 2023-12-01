e54391efeb3d0ddd46bc8570d088e89d69507efe
Gabriel Koehler da Silva <gabrielks@weg.net>
2023-12-01 19:56:01 -0300

    card list finalizado

--- src/components/CardTarefaList.vue
+++ src/components/CardTarefaList.vue
@@ -1,13 +1,19 @@
 <template>
 <div class='max-w-[1760px] w-max'>
     <div :style="distribuicao" class='h-[50px] items-center ml-9 grid'>
-        <div v-for="propriedade in tarefas[0]" class='border-r-2 last:border-none border-black px-4 truncate'>
-            {{propriedade.propriedade}}
+        <div v-for="propriedade in propriedades" class='border-r-2 last:border-none text-center border-black px-4 truncate'>
+            {{propriedade}}
         </div>
     </div>
     
-    <div  @drag="drag(item)" v-for="item in tarefas" class='card mb-2 flex'>
-        <div class="pl-4">
+    <div draggable='true' 
+    @dragstart="drag(tarefa,index)" 
+    @drop="dropTarefa(index)" 
+    @dragenter.prevent
+    @dragover.prevent
+    v-for="(tarefa,index) in tarefas" 
+    class='dropZone card mb-2 flex'>
+        <div handle class="pl-4">
             <svg  width="19" height="33" viewBox="0 0 19 33" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect y="0.973022" width="7.42971" height="7.68505" rx="1" fill="#FEFBFF"/>
             <rect x="11.4297" y="0.9729" width="7.42971" height="7.68505" rx="1" fill="#FEFBFF"/>
@@ -18,8 +24,8 @@
             </svg>
         </div>
         <div :style="distribuicao" class='grid w-full '>
-                <div v-for="propriedade in item" class=' border-r-2 text-center last:border-none border-white px-4 truncate'>
-                    {{propriedade.valor}} 
+                <div v-for="valor in tarefa.valores" class=' border-r-2 text-center last:border-none border-white px-4 truncate'>
+                    {{valor}} 
                 </div>
         </div>
     </div>
@@ -27,19 +33,49 @@
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import {ref, onMounted} from 'vue'
 const props=defineProps({
     tarefas:[]
-
 })
 
-console.log(props.tarefas[0].length)
+onMounted(() => {
+    propriedades.value=props.tarefas[0].propriedades
+})
+
+let propriedades=ref([])
+let tarefaMovida=ref({})
+let tarefaMovidaIndex=ref(0)
+
 
 const distribuicao={
-    gridTemplateColumns: "repeat("+props.tarefas[0].length+", minmax(0, 1fr))"
+    gridTemplateColumns: "repeat("+props.tarefas[0].propriedades.length+", minmax(0, 1fr))"
 }
-function drag(item) {
-    console.log(item)
+function drag(tarefa,i) {
+    
+    // console.log(tarefa)
+    tarefaMovida.value=tarefa
+    // console.log(i)
+    tarefaMovidaIndex.value=i
+    console.log(tarefaMovida)
+}
+function dropTarefa(i){
+//  e.preventDefault()
+
+ console.log(i)
+//  props.tarefas[tarefaMovidaIndex.value]=props.tarefas[i]
+//  props.tarefas[i]=tarefaMovida.value
+for (let i of props.tarefas) {
+ if (i == tarefaMovida.value) {
+   console.log(props.tarefas)
+   props.tarefas.splice(props.tarefas.indexOf(i), 1)
+   console.log(props.tarefas)
+ }
+}
+ props.tarefas.splice(i,0,tarefaMovida.value)
+ console.log(tarefaMovidaIndex.value)
+ console.log(tarefaMovidaIndex)
+ console.log(tarefaMovidaIndex.value)
+
 }
 </script>
 
