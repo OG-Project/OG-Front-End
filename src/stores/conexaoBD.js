6eec32e9e0780df7b9a97e7140995b9dcba08600
henriqueGJesus <hj30032006@gmail.com>
2024-04-12 14:00:48 -0300

    tanto faz

--- src/stores/conexaoBD.js
+++ src/stores/conexaoBD.js
@@ -6,7 +6,7 @@ import { webSocketStore } from "./webSocket.js";
 export const conexaoBD = defineStore('conexaoBD', {
   
     state: () => {
-      return {api:axios.get("http://localhost:8084/projeto")
+      return {api:axios.get("http://10.4.96.35:8082/projeto")
     }
 
     },
@@ -14,44 +14,44 @@ export const conexaoBD = defineStore('conexaoBD', {
     
       procurar(textoRequisicao){
         // return axios.get("http://10.4.96.35:8082"+ textoRequisicao).then(response => response.data)
-        return axios.get("http://localhost:8084"+ textoRequisicao).then(response => response.data)
+        return axios.get("http://10.4.96.35:8082"+ textoRequisicao).then(response => response.data)
       },
       cadastrar(objeto, textoRequisicao){
-        return axios.post("http://localhost:8084"+textoRequisicao,objeto)
+        return axios.post("http://10.4.96.35:8082"+textoRequisicao,objeto)
       },
       atualizar(objeto,textoRequisicao){
         
-        return axios.put("http://localhost:8084"+textoRequisicao,objeto).then(response =>{
+        return axios.put("http://10.4.96.35:8082"+textoRequisicao,objeto).then(response =>{
           
         })
       },
       async adicionarUsuarios(ids,equipeId,textoRequisicao){
-        axios.patch(`http://localhost:8084${textoRequisicao}/${equipeId}`, ids).then(response =>{
+        axios.patch(`http://10.4.96.35:8082${textoRequisicao}/${equipeId}`, ids).then(response =>{
           return response;
         })
         
       },
       deletarEquipe(id,textoRequisicao){
-        return axios.delete(`http://localhost:8084${textoRequisicao}/${id}`).then(response =>{
+        return axios.delete(`http://10.4.96.35:8082${textoRequisicao}/${id}`).then(response =>{
           
         })
       },
       async buscarMembrosEquipe(equipeId,textoRequisicao){
-          return await ((await axios.get(`http://localhost:8084${textoRequisicao}/${equipeId}`)).data)
+          return await ((await axios.get(`http://10.4.96.35:8082${textoRequisicao}/${equipeId}`)).data)
       },
       removerUsuarioDaEquipe(equipeId,userId,textoRequisicao){
-          return axios.delete(`http://localhost:8084${textoRequisicao}/${equipeId}/${userId}`).then(response =>{
+          return axios.delete(`http://10.4.96.35:8082${textoRequisicao}/${equipeId}/${userId}`).then(response =>{
            
           })
       },
       async buscarUm(id,textoRequisicao){
-        return (await axios.get('http://localhost:8084'+textoRequisicao+'/'+id).then(response => response.data))
+        return (await axios.get('http://10.4.96.35:8082'+textoRequisicao+'/'+id).then(response => response.data))
       },
       async buscarProjetosEquipe(equipeId, textoRequisicao){
-        return await ((await axios.get(`http://localhost:8084${textoRequisicao}/${equipeId}`)).data)
+        return await ((await axios.get(`http://10.4.96.35:8082${textoRequisicao}/${equipeId}`)).data)
       },
       async buscarProjetosUsuario(userId, textoRequisicao){
-        return await ((await axios.get(`http://localhost:8084${textoRequisicao}/${userId}`)).data)
+        return await ((await axios.get(`http://10.4.96.35:8082${textoRequisicao}/${userId}`)).data)
 
       },
       async cadastrarFoto(equipeId, foto) {
@@ -62,7 +62,7 @@ export const conexaoBD = defineStore('conexaoBD', {
     
             // Faça a requisição PATCH para enviar a imagem
     
-            const response = await axios.patch(`http://localhost:8084/equipe/${equipeId}`, formData, {
+            const response = await axios.patch(`http://10.4.96.35:8082/equipe/${equipeId}`, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
@@ -76,7 +76,7 @@ export const conexaoBD = defineStore('conexaoBD', {
             console.error('Erro ao cadastrar a foto:', error);
             throw error;
         }
-          return await ((await axios.get(`http://localhost:8084${textoRequisicao}/${equipeId}`)).data)
+          return await ((await axios.get(`http://10.4.96.35:8082${textoRequisicao}/${equipeId}`)).data)
       },
       
       
