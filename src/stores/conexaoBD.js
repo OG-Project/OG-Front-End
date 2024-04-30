370085d9650d73dc3b4bde24ba4dde0d0a384d77
henriqueGJesus <hj30032006@gmail.com>
2024-04-30 07:31:31 -0300

    merge

--- src/stores/conexaoBD.js
+++ src/stores/conexaoBD.js
@@ -8,63 +8,66 @@ export const conexaoBD = defineStore('conexaoBD', {
   
     state: () => {
 
-      return {api:axios.get("http://localhost:8085/projeto")
+      return {api:axios.get("http://localhost:8082/projeto")
     }
 
   },
   actions: {
       procurar(textoRequisicao){
-        return axios.get("http://localhost:8085"+ textoRequisicao).then(response => response.data)
+        return axios.get("http://localhost:8082"+ textoRequisicao).then(response => response.data)
+      },
+      login(usuarioLogin){
+        return axios.post("http://localhost:8082/login", usuarioLogin).then(response => console.log(response));
       },
       cadastrar(objeto, textoRequisicao){
-        return axios.post("http://localhost:8085"+textoRequisicao,objeto).then(response => response)
+        return axios.post("http://localhost:8082"+textoRequisicao,objeto).then(response => response)
       },
       atualizar(objeto,textoRequisicao){
         
-        return axios.put("http://localhost:8085"+textoRequisicao,objeto).then(response => response)
+        return axios.put("http://localhost:8082"+textoRequisicao,objeto).then(response => response)
       },
       adicionarUsuarios(ids,equipeId,textoRequisicao){
-        return axios.patch('http://localhost:8085'+textoRequisicao+'/'+equipeId,ids)
+        return axios.patch('http://localhost:8082'+textoRequisicao+'/'+equipeId,ids)
       },
       deletar(id,textoRequisicao){
-        return axios.delete(`http://localhost:8085${textoRequisicao}/${id}`).then(response =>{
+        return axios.delete(`http://localhost:8082${textoRequisicao}/${id}`).then(response =>{
           
         })
       },
       deletarProjetoEquipe(id,idProjeto,textoRequisicao){
-        return axios.delete(`http://localhost:8085${textoRequisicao}/${id}/${idProjeto}`).then(response =>{
+        return axios.delete(`http://localhost:8082${textoRequisicao}/${id}/${idProjeto}`).then(response =>{
           
         })
       },
       async buscarMembrosEquipe(equipeId,textoRequisicao){
-          return await ((await axios.get(`http://localhost:8085${textoRequisicao}/${equipeId}`)).data)
+          return await ((await axios.get(`http://localhost:8082${textoRequisicao}/${equipeId}`)).data)
       },
       removerUsuarioDaEquipe(equipeId,userId,textoRequisicao){
-          return axios.delete(`http://localhost:8085${textoRequisicao}/${equipeId}/${userId}`).then(response =>{
+          return axios.delete(`http://localhost:8082${textoRequisicao}/${equipeId}/${userId}`).then(response =>{
            
           })
       },
       async buscarUm(id,textoRequisicao){
 
-        return (await axios.get('http://localhost:8085'+textoRequisicao+'/'+id).then(response => response.data))
+        return (await axios.get('http://localhost:8082'+textoRequisicao+'/'+id).then(response => response.data))
       },
       async buscarProjetosEquipe(equipeId, textoRequisicao){
-        return await ((await axios.get(`http://localhost:8085${textoRequisicao}/${equipeId}`)).data)
+        return await ((await axios.get(`http://localhost:8082${textoRequisicao}/${equipeId}`)).data)
       },
       async buscarProjetosUsuario(userId, textoRequisicao){
-        return await ((await axios.get(`http://localhost:8085${textoRequisicao}/${userId}`)).data)
+        return await ((await axios.get(`http://localhost:8082${textoRequisicao}/${userId}`)).data)
 
 
       },
     adicionarEquipe(equipeId,projetoId, textoRequisicao) {
-      return axios.patch('http://localhost:8085' + textoRequisicao + '/' + projetoId + '/' + equipeId)
+      return axios.patch('http://localhost:8082' + textoRequisicao + '/' + projetoId + '/' + equipeId)
     },
     deletarEquipe(id, textoRequisicao) {
-      return axios.delete('http://localhost:8085'+textoRequisicao+'/'+id)    
+      return axios.delete('http://localhost:8082'+textoRequisicao+'/'+id)    
     },
 
     async deletarTarefa(textoRequisicao, id) {
-      return await axios.delete(`http://localhost:8085${textoRequisicao}/${id}`).then(response => {
+      return await axios.delete(`http://localhost:8082${textoRequisicao}/${id}`).then(response => {
       })
     },
     async cadastrarFoto(equipeId, foto) {
@@ -76,7 +79,7 @@ export const conexaoBD = defineStore('conexaoBD', {
 
         // Faça a requisição PATCH para enviar a image
 
-        const response = await axios.patch(`http://localhost:8085/equipe/${equipeId}`, formData, {
+        const response = await axios.patch(`http://localhost:8082/equipe/${equipeId}`, formData, {
           headers: {
             'Content-Type': 'multipart/form-data'
           }
@@ -96,7 +99,7 @@ export const conexaoBD = defineStore('conexaoBD', {
     async patchDeArquivosNaTarefa(arquivos, id) {
       try {
         // Deleta os arquivos existentes relacionados à tarefa
-        await axios.delete(`http://localhost:8085/tarefa/arquivos/${id}`);
+        await axios.delete(`http://localhost:8082/tarefa/arquivos/${id}`);
     
             const response = await axios.patch("http://localhost:8082" + textoRequisicao + "/" + id, formData, {
                 headers: {
@@ -110,7 +113,7 @@ export const conexaoBD = defineStore('conexaoBD', {
             console.error('Erro ao cadastrar a foto:', error);
             throw error;
         }
-          return await ((await axios.get(`http://localhost:8085${textoRequisicao}/${equipeId}`)).data)
+          return await ((await axios.get(`http://localhost:8082${textoRequisicao}/${equipeId}`)).data)
       },
       async cadastrarFotoUsuario(idUsuario, foto) {
         try {
@@ -119,7 +122,7 @@ export const conexaoBD = defineStore('conexaoBD', {
             formData.append('foto', foto);
     
             // Faça a requisição PATCH para enviar a imagem
-            const response = await axios.patch(`http://localhost:8085/usuario/${idUsuario}`, formData, {
+            const response = await axios.patch(`http://localhost:8082/usuario/${idUsuario}`, formData, {
 
                 headers: {
                     'Content-Type': 'multipart/form-data'
