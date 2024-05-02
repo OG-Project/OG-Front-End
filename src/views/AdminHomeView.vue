625d0c5d1e76a8c873b684a0b0497d007daa38cf
NicolasStassun <nicolas_stassun@estudante.sesisenai.org.br>
2024-05-02 00:17:42 -0300

    fix: tela de Adm do projeto já ta fazendo interações com uma lista de placeholer, porém os gráficos ja funcionam

--- src/views/AdminHomeView.vue
+++ src/views/AdminHomeView.vue
@@ -1,23 +1,42 @@
 <template>
-    <div class="h-[90vh] w-full">
+    <div class="h-[92vh] w-full">
         <div class="flex p-2 text-4xl justify-center">
             <h1>Lista de Tarefas</h1>
         </div>
         <div class="flex items-center relative justify-center mt-4 h-[38%] ml-16">
             <div id="poligono"
-                class="h-[100%] w-[40vh] shadow-2xl flex justify-center flex-col left-10 absolute overflow-visible"
+                class="h-[100%] w-[40vh] shadow-2xl flex justify-center flex-col left-[-0.50rem] absolute overflow-visible"
                 style="box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px; z-index: 5;">
+                <p class="flex justify-center items-center text-2xl">Tarefas do seu projeto</p>
             </div>
-            <div class="bg-[var(--backgroundItems)] ml-12 w-[76%] h-[92%] flex items-center justify-end"
+            <div class="bg-[var(--backgroundItems)] ml-8 w-[90%] h-[92%] flex items-center justify-end"
                 style="box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px">
-                <div class="overflow-y-auto h-[100%] w-[75%] flex items-center flex-col">
-                    <div class="flex gap-12 mt-16 flex-wrap justify-center w-[70%] text-4xl">
-
+                <div class="overflow-y-auto h-[100%] w-[80%] flex items-center flex-col">
+                    <div class="flex gap-12 mt-6 flex-wrap justify-center w-[100%] text-md">
+                        <div class="flex gap-14 w-[80%] items-center justify-center">
+                            <p class="w-[16%] flex items-center" >Nome da tarefa</p>
+                            <p class="w-[16%] flex items-center" >Lider</p>
+                            <p class="w-[16%] flex items-center" >Membros</p>
+                            <p class="w-[16%] flex items-center" >Status</p>
+                            <p class="w-[16%] flex items-center" >Tempo trabalhado</p>
+                            <p class="w-[16%] flex items-center" >Entrega</p>
+                        </div>
+                        <div v-for="tarefa of tarefas" class="w-[80%] text-md">
+                            <div class="flex gap-14">
+                                <p class="w-[16%] truncate flex items-center justify-center h-8 bg-[#B488D7]" >{{ tarefa.nome }}</p>
+                                <p class="w-[16%] flex items-center justify-center h-8" >{{ tarefa.lider }}</p>
+                                <p class="w-[16%] flex items-center justify-center h-8 bg-[#8A59B1]" >{{ tarefa.membros.length }}</p>
+                                <p class="w-[16%] flex items-center justify-center h-8 bg-[#C6B473]" v-if="tarefa.status == 'Em progresso'">{{ tarefa.status }}</p>
+                                <p class="w-[16%] flex items-center justify-center h-8 bg-[#7CC0E5]" v-if="tarefa.status == 'Pronta'">{{ tarefa.status }}</p>
+                                <p class="w-[16%] flex items-center justify-center h-8 bg-[#93E28D]" >{{ tarefa.horas }}</p>
+                                <p class="w-[16%] flex items-center justify-center h-8 bg-[#EF8F7A]">{{ format(new Date(tarefa.diaCompleto), "dd/MM/yyyy") }}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="flex items-center relative justify-center mt-4 h-[38%]">
+        <div class="flex items-center relative justify-center mt-16 h-[38%]">
             <div class="flex pl-12 flex-col justify-center w-[15%] h-[85%] border-r-2 border-black">
                 <p class="text-2xl">Tarefas</p>
                 <p>Total: {{ totalDeTarefas }}</p>
@@ -26,120 +45,180 @@
                 <p>Horas trabalhadas: {{ totalHorasTrabalhadas }}</p>
             </div>
             <div class="flex flex-col w-[5%] h-full items-center justify-center gap-4">
-                <button @click="changeChart('bar')" class="bg-[var(--roxoEscuro)] h-14 w-[3.5rem] rounded-xl flex justify-center items-center">
+                <button @click="changeChart('bar')"
+                    class="bg-[var(--roxoEscuro)] h-14 w-[3.5rem] rounded-xl flex justify-center items-center">
                     <img src="../assets/GraficoBarraVertical.svg" />
                 </button>
-                <button @click="changeChart('horizontalBar')" class="bg-[var(--roxoEscuro)] h-14 w-[3.5rem] rounded-xl flex justify-center items-center">
+                <button @click="changeChart('horizontalBar')"
+                    class="bg-[var(--roxoEscuro)] h-14 w-[3.5rem] rounded-xl flex justify-center items-center">
                     <img src="../assets/GraficoHorizontal.svg" />
                 </button>
-                <button @click="changeChart('line')" class="bg-[var(--roxoEscuro)] h-14 w-[3.5rem] rounded-xl flex justify-center items-center">
+                <button @click="changeChart('line')"
+                    class="bg-[var(--roxoEscuro)] h-14 w-[3.5rem] rounded-xl flex justify-center items-center">
                     <img src="../assets/GraficoDeLinha.svg" />
                 </button>
             </div>
             <div class="flex flex-col w-[60%] h-full">
-                <canvas ref="chart" class="h-full"></canvas>
+                <canvas id="chart" class="h-full"></canvas>
             </div>
         </div>
         <div class="flex items-center justify-center">
-            <div class="w-[80%] h-[30%] justify-center flex items-center">
+            <!-- <div class="w-[80%] h-[30%] justify-center flex items-center">
                 <Carousel :value="tarefas.value" :numVisible="3" containerClass="" contentClass="w-[100%] h-[100%]"
                     indicatorsContentClass="*:bg-gray-300 active:*:bg-roxo w-full" :showNavigators="true"
                     :showIndicators="true" verticalViewPortHeight="500px" :numScroll="3" :responsiveOptions="projetos">
                     <template #item="tarefa">
-                        <!-- {{ slotProps.index }} -->
                         <div class="bg-orange-600">
                             <p>{{ tarefa.nome }}</p>
                         </div>
-                        <!-- {{ slotProps.index }} -->
                     </template>
                 </Carousel>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
 <script setup>
-import { ref, onMounted } from 'vue'
+import { format } from "date-fns";
+import { ref, onMounted, computed } from 'vue'
 import Chart from 'chart.js/auto'
-
 let chart = null
 
 onMounted(() => {
-    renderChart('bar')
+    renderChart('line')
 })
 
+let tarefas = ref([
+    {
+        nome: 'Tarefa 1',
+        lider: 'João',
+        membros: ['Maria', 'Pedro'],
+        horas: 3,
+        status: 'Pronta',
+        diaCompleto: new Date('2021-10-10')
+    },
+    {
+        nome: 'Tarefa 2',
+        lider: 'Ana',
+        membros: ['Carlos', 'Lúcia'],
+        horas: 1,
+        status: 'Pronta',
+        diaCompleto: new Date('2021-10-10')
+    },
+    {
+        nome: 'Tarefa 3',
+        lider: 'José',
+        membros: ['Fernanda', 'Rafael'],
+        horas: 4,
+        status: 'Em progresso',
+        diaCompleto: new Date('2021-10-10')
+    },
+    {
+        nome: 'Tarefa 4',
+        lider: 'Paula',
+        membros: ['Daniel', 'Juliana'],
+        horas: 2,
+        status: 'Em progresso',
+        diaCompleto: new Date('2021-10-10')
+    },
+    {
+        nome: 'Tarefa 5',
+        lider: 'Lucas',
+        membros: ['Camila', 'Thiago'],
+        horas: 3,
+        status: 'Pronta',
+        diaCompleto: new Date('2021-10-10')
+    },
+    {
+        nome: 'Tarefa 6',
+        lider: 'Carla',
+        membros: ['Gustavo', 'Amanda'],
+        horas: 1,
+        status: 'Pronta',
+        diaCompleto: new Date('2021-10-10')
+    },
+    {
+        nome: 'Tarefa 7',
+        lider: 'Roberto',
+        membros: ['Patrícia', 'Eduardo'],
+        horas: 4,
+        status: 'Em progresso',
+        diaCompleto: new Date('2021-10-10')
+    },
+]);
+
 function renderChart(type) {
-    const ctx = document.getElementById('chart').getContext('2d')
+    let options
     if (chart) {
-        chart.destroy()
+        chart.destroy(); // Destruir o gráfico existente se existir
     }
-    chart = new Chart(ctx, {
-        type: type,
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
+    if (type == 'horizontalBar') {
+        type = 'bar'
+        options = {
+            indexAxis: 'y',
             scales: {
-                y: {
-                    beginAtZero: true
+                x: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Horas'
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
                 }
             }
         }
-    })
+    } else {
+        options = {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Horas'
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    }
+
+    chart = new Chart(
+        document.getElementById('chart'), {
+        type: type,
+        data: {
+            labels: tarefas.value.map(tarefa => tarefa.nome),
+            datasets: [{
+                label: 'Horas por Tarefa',
+                data: tarefas.value.map(tarefa => tarefa.horas),
+                backgroundColor: tarefas.value.map((tarefa, index) => {
+                    return tarefa.status === 'Pronta' ? 'rgba(54, 162, 235, 0.2)' : 'rgba(255, 99, 132, 0.2)';
+                }),
+                borderColor: tarefas.value.map((tarefa, index) => {
+                    return tarefa.status === 'Pronta' ? 'rgba(54, 162, 235, 1)' : 'rgba(255, 99, 132, 1)';
+                }),
+                borderWidth: 1
+            }]
+        },
+        options: options
+    }
+    );
 }
 
 function changeChart(type) {
     renderChart(type)
 }
-
-let tarefas = ref([
-    { nome: 'Tarefa 1', horas: 2, status: 'Em progresso' },
-    { nome: 'Tarefa 2', horas: 3, status: 'Pronta' },
-    { nome: 'Tarefa 3', horas: 1, status: 'Pronta' },
-    { nome: 'Tarefa 4', horas: 4, status: 'Em progresso' },
-    { nome: 'Tarefa 5', horas: 2, status: 'Em progresso' },
-    { nome: 'Tarefa 6', horas: 3, status: 'Pronta' },
-    { nome: 'Tarefa 7', horas: 1, status: 'Pronta' },
-    { nome: 'Tarefa 8', horas: 4, status: 'Em progresso' },
-    { nome: 'Tarefa 9', horas: 2, status: 'Em progresso' },
-    { nome: 'Tarefa 10', horas: 3, status: 'Pronta' },
-    { nome: 'Tarefa 11', horas: 1, status: 'Pronta' },
-    { nome: 'Tarefa 12', horas: 4, status: 'Em progresso' },
-    { nome: 'Tarefa 13', horas: 2, status: 'Em progresso' },
-    { nome: 'Tarefa 14', horas: 3, status: 'Pronta' },
-    { nome: 'Tarefa 15', horas: 1, status: 'Pronta' },
-    { nome: 'Tarefa 16', horas: 4, status: 'Em progresso' },
-    { nome: 'Tarefa 17', horas: 2, status: 'Em progresso' },
-    { nome: 'Tarefa 18', horas: 3, status: 'Pronta' },
-    { nome: 'Tarefa 19', horas: 1, status: 'Pronta' },
-    { nome: 'Tarefa 20', horas: 4, status: 'Em progresso' },
-
-])
-
-let totalDeTarefas = ref()
-let totalEmProgresso = ref()
-let totalProntas = ref()
-let totalHorasTrabalhadas = ref()
+//aqui é onde eu faço a contagem de tarefas, horas trabalhadas, tarefas em progresso e prontas
+let totalDeTarefas = computed(() => tarefas.value.length)
+let totalEmProgresso = computed(() => tarefas.value.filter(tarefa => tarefa.status === 'Em progresso').length)
+let totalProntas = computed(() => tarefas.value.filter(tarefa => tarefa.status === 'Pronta').length)
+let totalHorasTrabalhadas = computed(() => tarefas.value.reduce((acc, tarefa) => acc + tarefa.horas, 0))
 </script>
 <style scoped>
 #poligono {
