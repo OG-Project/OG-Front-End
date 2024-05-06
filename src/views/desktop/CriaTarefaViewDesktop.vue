afdd6b2610ec1ed71b716581be0b7fa3602b839c
NicolasStassun <nicolas_stassun@estudante.sesisenai.org.br>
2024-05-06 13:32:11 -0300

    pull

--- src/views/desktop/CriaTarefaViewDesktop.vue
+++ src/views/desktop/CriaTarefaViewDesktop.vue
@@ -1208,7 +1208,7 @@ function formatarData(data) {
     const ano = dataObj.getFullYear();
     const horas = String(dataObj.getHours()).padStart(2, '0');
     const minutos = String(dataObj.getMinutes()).padStart(2, '0');
-    return `${ano}/${mes}/${dia} - ${horas}:${minutos}`;
+    return `${dia}/${mes}/${ano} - ${horas}:${minutos}`;
   }
   else {
     return "Data não especificada"
